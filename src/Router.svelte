41dceac807d76e7234815a8e428baba1e78fb4b5
Deepareenzeer <kittisak.phao@ku.th>
2025-11-29 20:21:14 +0700

    Refactor: Migrate project structure to SvelteKit

--- src/Router.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { Router, Route } from "svelte-routing";
-  import TestRoute from "./routes/Home.svelte"; 
-</script>
-
-<Router url="/">
-  <Route path="/" component={TestRoute} /> 
-  
-  </Router>
